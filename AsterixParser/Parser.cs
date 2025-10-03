95ee665cc19600b8257f0531817c8d9f7f509290
Jan Viñas Corella <mirabetjan@gmail.com>
2025-10-03 11:57:33 +0200

    función parseo (wip)

--- AsterixParser/Parser.cs
+++ AsterixParser/Parser.cs
@@ -5,7 +5,16 @@
 
         public static void ParseFile(byte[] file)
         {
+            int i = 0;
 
+            while (i < file.Length)
+            {
+                byte cat = file[i];
+                ushort length =  (ushort) (file[i+2] | (file[i+1] << 8));
+                Console.WriteLine(length);
+
+                i += length;
+            }
         }
     }
 }
