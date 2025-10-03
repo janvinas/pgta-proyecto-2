c79496dbe5cdb668131f89304282ad7bf3b7aef7
Paugamer <pauserranor@gmail.com>
2025-10-03 18:36:51 +0200

    FSPEC

---
+++ AsterixParser/CAT48.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsterixParser
+{
+    internal class CAT48(byte[] body)
+    {
+        public int CAT48Reader(int i, ushort length) 
+        {
+            int error = 0;
+            
+            int errorFSPEC = FSPECReader();
+            
+            return error;
+        }
+
+
+        public int FSPECReader()
+        {
+            int error = 0;
+
+            int k = 0;
+            bool fx = true;
+            while (fx) {
+                byte b = body[k];
+                for (int i = 7; i >= 0; i--) Console.Write((b >> i) & 1);
+
+                int bit = b & 1;
+                if (bit == 0) fx = false;
+
+                k++;
+            }
+            Console.WriteLine();
+            Console.WriteLine(k);
+            return error;
+        }
+
+    }
+}
--- AsterixParser/Parser.cs
+++ AsterixParser/Parser.cs
@@ -5,14 +5,44 @@
 
         public static void ParseFile(byte[] file)
         {
-            int i = 0;
+            CAT21 CAT021;
+            CAT48 CAT048;
 
-            while (i < file.Length)
+            int error = 0;
+            
+
+            int i = 0;
+            int k = 0;
+
+            while (i < file.Length && k<3)
             {
                 byte cat = file[i];
                 ushort length =  (ushort) (file[i+2] | (file[i+1] << 8));
+
+                //Body es el cuerpo sin CAT ni LEN
+                byte[] body = new byte[length - 3];
+                Array.Copy(file,i+3,body,0,length-3);
+                
                 Console.WriteLine(length);
 
+                switch (cat)
+                {
+                    case 21:
+                        Console.WriteLine("CAT-"+cat);
+                        
+                        CAT021 = new CAT21(body);
+                        error = CAT021.CAT21Reader(i, length);
+                        
+                        break;
+                    case 48:
+                        Console.WriteLine("CAT-"+cat);
+
+                        CAT048 = new CAT48(body);
+                        error = CAT048.CAT48Reader(i, length);
+                        
+                        break;
+                }
+                k ++;
                 i += length;
             }
         }
