aeecec69a1d85f20c35d541066e0d93beebfa686
ivanpascuaspalma <ivanpascuaspalma@gmail.com>
2025-10-04 20:06:25 +0200

    Creado DataParser21 y actualizado CAT21.cs

---
+++ AsterixParser/DataItemParser21.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AsterixParser
+{
+    internal class DataItemParser21
+    {
+        /*Jan decia de hacerlo asi:
+        public static Action[] functions = {}
+        */
+        //Para poner el valor de "k" y poderlo modificar se necesita poner ref y por lo cual crear el delegate 
+        public delegate void DelegateFunctions(ref int k, byte[] body);
+
+        public static DelegateFunctions[] functions = {
+            
+
+        };
+    }
+}
