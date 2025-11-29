1d09bf47cf875c34ba4df527d06599f5fcacccff
Jan Viñas Corella <mirabetjan@gmail.com>
2025-11-29 11:06:13 +0100

    pasar automaticamente a tabla y añadir icono

--- AsterixViewer/MainWindow.xaml.cs
+++ AsterixViewer/MainWindow.xaml.cs
@@ -55,6 +55,7 @@ namespace AsterixViewer
                 var result = await Parser.ParseFileAsync(data, progress);
                 ((App)Application.Current).DataStore.Messages = result.messages;
                 ((App)Application.Current).DataStore.Flights = result.flights;
+                NavigationView.Navigate(typeof(MapTab));
 
                 //FinishedLoadingFile?.Invoke(this, EventArgs.Empty);
             }
